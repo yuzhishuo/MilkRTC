0516974eff780b78c8350b8dc5a2a2281a6d1d1d
muduo <aoumeior@outlook.com>
2020-05-06 16:47:44 +0800

    👌

--- server_example/server.js
+++ server_example/server.js
@@ -1,5 +1,5 @@
 // Load required modules
-var http    = require("http");              // http server core module
+var http = require("http");              // http server core module
 var express = require("express");           // web framework external module
 var serveStatic = require('serve-static');  // serve static files
 var socketIo = require("socket.io");        // web socket external module
@@ -16,53 +16,60 @@ process.title = "node-easyrtc";
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var app = express();
-app.use(serveStatic('static', {'index': ['index.html']}));
+app.use(serveStatic('static', { 'index': ['index.html'] }));
 
 // Start Express http server on port 8080
 var webServer = http.createServer(app);
 
 // Start Socket.io so it attaches itself to Express server
-var socketServer = socketIo.listen(webServer, {"log level":1});
+var socketServer = socketIo.listen(webServer, { "log level": 1 });
 
 easyrtc.setOption("logLevel", "debug");
 
 // Overriding the default easyrtcAuth listener, only so we can directly access its callback
-easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
-    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
+easyrtc.events.on("easyrtcAuth", function (socket, easyrtcid, msg, socketCallback, callback) {
+    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function (err, connectionObj) {
         if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
             callback(err, connectionObj);
             return;
         }
 
-        connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
+        connectionObj.setField("credential", msg.msgData.credential, { "isShared": false });
 
-        console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
+        console.log("[" + easyrtcid + "] Credential saved!", connectionObj.getFieldValueSync("credential"));
 
         callback(err, connectionObj);
     });
 });
 
 // To test, lets print the credential to the console for every room join!
-easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
-    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+easyrtc.events.on("roomJoin", function (connectionObj, roomName, roomParameter, callback) {
+    console.log("[" + connectionObj.getEasyrtcid() + "] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
     easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
 });
 
 // Start EasyRTC server
-var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
+easyrtc.listen(app, socketServer, null, function (err, rtcRef) {
     console.log("Initiated");
 
-    rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+    rtcRef.events.on("roomCreate", function (appObj, creatorConnectionObj, roomName, roomOptions, callback) {
         console.log("roomCreate fired! Trying to create: " + roomName);
 
         appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
     });
 });
 
-// easyrtc.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback)
-// {
 
-// });
+easyrtc.on("authenticate", function (socket, easyrtcid, appName, username, credential, easyrtcAuthMessage, next) {
+    next(null);
+});
+
+easyrtc.on("roomCreate", function (appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+
+    easyrtc.util.logDebug("[" + appObj.getAppName() + "]" + (creatorConnectionObj ? "[" + creatorConnectionObj.getEasyrtcid() + "]" : "") + " Room [" + roomName + "] Running func 'onRoomCreate'");
+    easyrtc.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback)
+    callback(null);
+});
 
 // Listen on port 8080
 webServer.listen(8080, function () {
